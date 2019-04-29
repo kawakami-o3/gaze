101509a03dadd9647346e892d7b0d57f1d5e8f4a
kawakami <kawakami.ozone@gmail.com>
2019-04-30 03:35:59 +0900

    Add a command handler.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os/exec"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
@@ -29,6 +30,52 @@ func addRetry(w *fsnotify.Watcher) {
 	}
 }
 
+// chWriter receives outputs from exec.Cmd.
+type chWriter struct {
+	ch chan string
+}
+
+func newChWriter(ch chan string) *chWriter {
+	return &chWriter{
+		ch: ch,
+	}
+}
+
+func (c *chWriter) Write(p []byte) (n int, err error) {
+	c.ch <- string(p)
+	return len(p), nil
+}
+
+type Command struct {
+	cmdStr string
+	cmd    *exec.Cmd
+	out    chan string
+}
+
+func NewCommand(cmd string) *Command {
+	return &Command{
+		cmdStr: cmd,
+		cmd:    nil,
+		out:    make(chan string, 10),
+	}
+}
+
+func (c *Command) Run() {
+	c.cmd = exec.Command(c.cmdStr)
+	writer := newChWriter(c.out)
+	c.cmd.Stdout = writer
+
+	// This goroutine will stop when cmd is killed.
+	go func() {
+		c.cmd.Run()
+	}()
+}
+
+func (c *Command) Kill() {
+	c.cmd.Process.Kill()
+	c.cmd.Wait()
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
@@ -44,13 +91,17 @@ func main() {
 	}
 	defer watcher.Close()
 
+	cmdTarget := NewCommand(command)
+	cmdTarget.Run()
+
 	done := make(chan bool)
 	go func() {
 		for {
 			select {
+			case s := <-cmdTarget.out:
+				fmt.Print(s)
 			case event, ok := <-watcher.Events:
 				if !ok {
-					fmt.Println("event panic")
 					return
 				}
 				log.Println("event:", event)
@@ -58,12 +109,14 @@ func main() {
 					log.Println("modified file:", event.Name)
 				}
 
+				cmdTarget.Kill()
 				addRetry(watcher)
+				cmdTarget.Run()
 			case err, ok := <-watcher.Errors:
 				if !ok {
-					fmt.Println("error panic")
 					return
 				}
+				cmdTarget.Kill()
 				log.Println("error:", err)
 			}
 		}
